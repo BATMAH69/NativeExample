5f603050bef6b344a11bc8d4ed8a46fb1130b583
batmah <dev-www@ya.ru>
2016-09-23 04:52:53 +0300

    Добавил вкладки для iOS

--- index.ios.js
+++ index.ios.js
@@ -9,46 +9,51 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  DrawerLayoutAndroid
 } from 'react-native';
+
 import Page from './src/Main';
+import { buttons } from './src/Navigation'
+
+const styles = StyleSheet.create({
+  tabContent: {
+    flex: 1,
+    alignItems: 'center',
+  },
+  tabText: {
+    color: 'white',
+    margin: 50,
+  },
+});
 
 class NativeExample extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = { color: 'green' };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <TabBarIOS
+        unselectedTintColor="yellow"
+        tintColor="white"
+        barTintColor="darkslateblue"
+      >
+        { buttons.map((item, key) => (
+          <TabBarIOS.Item
+            title={item.title}
+            systemIcon={item.icon}
+            selected={this.state.color === item.color}
+            onPress={() => this.setState({ color: item.color })}
+          >
+            <Page color={this.state.color} />
+          </TabBarIOS.Item>)
+        )}
+      </TabBarIOS>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('NativeExample', () => NativeExample);
